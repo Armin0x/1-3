abc5a7cd27bf96a7f639a2912916ff3d0a93763f
Armin <58628813+Armin0x@users.noreply.github.com>
2020-03-07 01:03:42 +0100

    Add files via upload

---
+++ 1-3/1-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter one number between 1 and 3");
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("You got a new car !");
+                    break;
+                case 2:
+                    Console.WriteLine("You got a new plane !");
+                    break;
+                case 3:
+                    Console.WriteLine("You got a new bike !");
+                    break;
+                default:
+                    Console.WriteLine("You have entered wrong number !");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
